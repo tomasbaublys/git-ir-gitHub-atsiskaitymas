a489dbaef93c08a423dfb8904f929cdf03ae8709
tomasbaublys <t.baublys@icloud.com>
2025-02-18 16:28:25 +0200

    Implemented TabManager for managing tabs

---
+++ modules/TabManager.js
@@ -0,0 +1,23 @@
+export default class TabManager {
+    constructor(tabs, contentContainer, buttonContainer) {
+        this.tabs = tabs;
+        this.contentContainer = contentContainer;
+        this.buttonContainer = buttonContainer;
+        this.activeTab = null;
+    }
+
+    initialize() {
+        this.tabs.forEach(tab => {
+            const button = new Button(tab, this.openTab.bind(this));
+            this.buttonContainer.appendChild(button.createButtonElement());
+        });
+    }
+
+    openTab(tab) {
+        if (this.activeTab) {
+            this.contentContainer.innerHTML = "";
+        }
+        this.activeTab = tab;
+        this.contentContainer.appendChild(tab.createTabElement());
+    }
+}
